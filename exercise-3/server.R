a3ce984cd7258c32d8b1fbf709d17463cee9adad
Richie Wang <wangw05@uw.edu>
2018-05-09 16:03:32 -0700

    add all answers

--- exercise-3/server.R
+++ exercise-3/server.R
@@ -6,12 +6,23 @@ library(ggplot2)
 
 # It should take as an input the x_var, y_var, color, and size
 # Save the result of `renderPlot` to output$scatter
-
-    # Store x and y values to plot
-
-
-    # Store the title of the graph in a variable
-
+server <- function(input, output){
+  # Store x and y values to plot
+  output$scatter <- renderPlot({
+  
+    x <- mpg[[input$x_var]]
+    y <- mpg[[input$y_var]]
     
-    # Create ggplot scatter
+    title <- paste0("MPG Data: ", x, "vs. ", y)
+    
+    p <- ggplot() +
+      geom_point(mapping = aes(x = x, y = y), 
+                 size = input$size, 
+                 color = input$color) +
+      labs(x = input$x_var, y = input$y_var, title = title)
+    p
+      
+  })
+}
 
+shinyServer(server)
--- exercise-3/ui.R
+++ exercise-3/ui.R
@@ -5,6 +5,19 @@ library(shiny)
 select_values <- colnames(mpg)
 
 # Create a shinyUI with a `fluidPage` layout
+shinyUI(fluidPage(
+  h1("MPG Dataset"),
+  
+  selectInput("x_var", label = "X Var", choices = select_values, selected = "displ"),
+  
+  selectInput("y_var", label = "Y Var", choices = select_values, selected = "cyl"),
+  
+  sliderInput("size", label = "Size", min = 1, max = 10, value = 5),
+  
+  selectInput("color", label = "Color", choices = c("Red" = "red", "Green" = "green", "Blue" = "blue")),
+  
+  plotOutput("scatter")
+  ))
 
   # A page header
 
